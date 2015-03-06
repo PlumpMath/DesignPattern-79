26edc25042985d45194a831342c6db4b863b16bb
pb <564825336@qq.com>
2015-03-06 10:32:15 +0800

    Second Commit

---
+++ DesignPattern/src/learn/design/pattern/prototype/xiaocai/Show.java
@@ -0,0 +1,16 @@
+package learn.design.pattern.prototype.xiaocai;
+
+public class Show {
+
+	public static void main(String[] args) {
+		Resume resume = new Resume("haha");
+		resume.SetPersonalInfo("man", "15");
+		resume.SetWorkExperience("1998-2000", "test");
+		
+		resume.Display();
+		
+		
+		
+	}
+
+}
